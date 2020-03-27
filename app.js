daa834a685d06b59f55358b335a0af23a1c01bb7
Gabrielle Ynara <gabrielleynara@ymail.com>
2020-03-27 18:02:57 -0500

    Passport JWT

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session); 
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +13,7 @@ const promotionRouter = require('./routes/promotionRouter');
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -40,34 +37,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321')); // signed cookie
 
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false, // when new session is created but there's no updates, it won get saved at the end of the session  
-    resave: false, // once a session has been created and updated in sync, it will continue to be saved
-    store: new FileStore() 
-}))
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// Defining authentication middleware
-function auth(req, res, next){ 
-    console.log(req.user);
-
-    if (!req.user) {// if the request doesn't have a signed cookie
-        const err = new Error('You\'re not authenticated');
-        err.status = 401; // Unauthorized
-        return next(err);
-    } else { 
-        return next();  
-    }
-}
-
-app.use(auth); // using authentication middleware
 
 app.use(express.static(path.join(__dirname, 'public')));
 
