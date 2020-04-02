e54b29b9f9671339428d21e13f5ce7120d23e882
Gabrielle Ynara <gabrielleynara@ymail.com>
2020-04-01 21:44:10 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '218580816027530',
+        clientSecret: 'a500e8a6a196c8d14cc9af2b2d4c0b24'
+    }
 }
